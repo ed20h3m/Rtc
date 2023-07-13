46838f2ef0f14463ad66969af1741bcf0e6793c1
Hesam Mousavi <=>
2023-07-13 16:28:46 +0100

    password pages refactored

--- client/src/Components/Pages/ForgetPassword/ForgetPassword.jsx
+++ client/src/Components/Pages/ForgetPassword/ForgetPassword.jsx
@@ -1,42 +1,32 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../../../context/Auth/AuthState";
-import Input from "../../utils/Input/Input";
+import ProfileImage from "../../utils/ProfileImage/ProfileImage";
 import Button from "../../utils/Button/Button";
+import Input from "../../utils/Input/Input";
+import Card from "../../utils/Card/Card";
 import "../ResetPassword/ResetPassword.scss";
 import "../../utils/utils.scss";
-import { AlertContext } from "../../../context/Alert/Alert";
-import Loading from "../../utils/Loading";
 
 const ForgetPassword = () => {
   const [email, setEmail] = useState("");
   const { ForgottenPassword } = useContext(AuthContext);
-  const { isLoading } = useContext(AlertContext);
   const OnChange = (e) => setEmail(e.target.value);
   const OnSend = () => {
     if (email === "") return;
     ForgottenPassword(email);
   };
+
   return (
-    <div className="con">
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div className="reset-password">
-          <header>
-            <h2>Forgotten Password</h2>
-          </header>
-          <main>
-            <p>
-              Enter the email associated with your account. We will send you an
-              email to reset your password.
-            </p>
-            <Input placeholder="Enter Your Email" onChange={OnChange} />
-          </main>
-          <footer>
-            <Button event={OnSend} name="Send Link" />
-          </footer>
+    <div className="center-con-column full-height-width">
+      <Card>
+        <h1>Forgotten Password ?</h1>
+        <ProfileImage link="https://cdn-icons-png.flaticon.com/512/1803/1803612.png" />
+        <div className="center-div-abs vertical-div width100">
+          <Input placeholder="Enter Email" onChange={OnChange} name="Email" />
         </div>
-      )}
+        <a href="/">Login</a>
+        <Button name="Send Link" event={OnSend} />
+      </Card>
     </div>
   );
 };
--- client/src/Components/Pages/ResetPassword/ResetPassword.jsx
+++ client/src/Components/Pages/ResetPassword/ResetPassword.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useContext, useState, Fragment } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import "./ResetPassword.scss";
 import Input from "../../utils/Input/Input";
 import Button from "../../utils/Button/Button";
 import { useParams, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../context/Auth/AuthState";
 import { AlertContext } from "../../../context/Alert/Alert";
-import Loading from "../../utils/Loading";
+import Card from "../../utils/Card/Card";
+import ProfileImage from "../../utils/ProfileImage/ProfileImage";
 
 const ResetPassword = () => {
   const { id, token } = useParams();
   const { ValidateToken, ResetPasswordFuc } = useContext(AuthContext);
-  const { isLoading, SetAlert } = useContext(AlertContext);
+  const { SetAlert } = useContext(AlertContext);
   const [state, setState] = useState({
     Password: "",
     Password2: "",
@@ -36,34 +37,24 @@ const ResetPassword = () => {
     } else SetAlert("Passwords do not Match.");
   };
   return (
-    <div className="con">
-      <div className="reset-password">
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <Fragment>
-            <header>
-              <h2>Reset Password</h2>
-            </header>
-            <main>
-              <h3>Enter New password</h3>
-              <Input
-                placeholder="Enter New Password"
-                name="Password"
-                onChange={OnInput}
-              />
-              <Input
-                placeholder="Confirm Password"
-                name="Password2"
-                onChange={OnInput}
-              />
-            </main>
-            <footer>
-              <Button name="Rest Password" event={OnClick} />
-            </footer>
-          </Fragment>
-        )}
-      </div>
+    <div className="center-con-column full-height-width">
+      <Card>
+        <h1>Reset Password</h1>
+        <ProfileImage link="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Eo_circle_light-blue_checkmark.svg/640px-Eo_circle_light-blue_checkmark.svg.png"/>
+        <div className="center-div-abs vertical-div width100">
+          <Input
+            placeholder="Enter New Password"
+            onChange={OnInput}
+            name="Password"
+          />
+          <Input
+            placeholder="Confirm New Password"
+            onChange={OnInput}
+            name="Password2"
+          />
+        </div>
+        <Button name="Reset Password" event={OnClick} />
+      </Card>
     </div>
   );
 };
